08f23fcb3bfcec571ccbb764d864e38d14e3863c
kevyncamacaro77 <kevyncamacaro77@gmail.com>
2026-05-02 10:19:26 -0400

    Bucle while y for

---
+++ ejercicio1.py
@@ -0,0 +1,19 @@
+x=3
+
+if x > 3:
+````print("x es positivo")
+
+elif x == 0
+    print("x es negativo o cero")
+
+# estructura de control for
+
+for i in range(3):
+    print(f"i={i}")
+
+# estructura de control while
+
+n = 3
+while n > 0:
+    print(n)
+    n -=1   
