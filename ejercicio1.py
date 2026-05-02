4eef8dcebbc2e9ebf250cd3263b9f184da596fa1
kevyncamacaro77 <kevyncamacaro77@gmail.com>
2026-05-02 10:28:17 -0400

    Prueba de los for y while

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,19 +1,18 @@
-x=3
+x = 3
 
-if x > 3:
-````print("x es positivo")
-
-elif x == 0
-    print("x es negativo o cero")
+if x > 0:
+    print("x es positivo")
+elif x == 0:
+    print("x es cero")
+else:
+    print("x es negativo")
 
 # estructura de control for
-
 for i in range(3):
     print(f"i={i}")
 
 # estructura de control while
-
 n = 3
 while n > 0:
     print(n)
-    n -=1   
+    n -= 1
